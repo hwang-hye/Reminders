02541d512d88db7afbb2c192921248a4cdbc7815
hwanghye <hwang.sine@gmail.com>
2024-07-03 02:23:42 +0900

    feat: TitleTextField Toast Message 구현

--- Reminders/Base/BaseViewController.swift
+++ Reminders/Base/BaseViewController.swift
@@ -20,3 +20,35 @@ class BaseViewController: UIViewController {
     }
     func configureLayout() { }
 }
+
+extension UIView {
+    func showToast(message: String, duration: Double = 2.0) {
+        let toastLabel = UILabel()
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center
+        toastLabel.font = UIFont.systemFont(ofSize: 14.0)
+        toastLabel.text = message
+        toastLabel.alpha = 0.0
+        toastLabel.numberOfLines = 0
+        
+        let textSize = toastLabel.intrinsicContentSize
+        let labelWidth = min(textSize.width + 20, self.frame.width - 40)
+        let labelHeight = textSize.height + 10
+        toastLabel.frame = CGRect(x: (self.frame.width - labelWidth) / 2, y: self.frame.height - 100, width: labelWidth, height: labelHeight)
+        
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        self.addSubview(toastLabel)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 1.0
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.5, delay: duration, options: .curveEaseIn, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: { _ in
+                toastLabel.removeFromSuperview()
+            })
+        })
+    }
+}
