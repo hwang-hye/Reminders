479533d9ee565dd44c27497d59325250f2a8017f
hwanghye <hwang.sine@gmail.com>
2024-07-02 22:44:46 +0900

    feat: BaseViewController 추가

---
+++ Reminders/Base/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  Reminders
+//
+//  Created by hwanghye on 7/2/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureView()
+        configureLayout()
+    }
+    func configureHierarchy() { }
+    func configureView() { }
+    func configureLayout() { }
+}
